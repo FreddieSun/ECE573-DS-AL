f064e35b46e823097e1d13e48cb56a7a1febbcba
FreddieSun <freddiesun95@gmail.com>
2018-02-14 13:51:34 -0500

    Assignment 2. Finish the Q1 and Q2

---
+++ Assignment2/src/Homework2_1.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Homework2_1 {
+
+	public static int shellosrt(int[] nums) {
+		int[] hSequence= {7,3,1};
+		int comNum=0;
+		for(int i=0;i<3;i++) {
+			int h=hSequence[i];
+			for(int j=h;j<nums.length;j++) {
+				for(int k=j;k>=h;k-=h) {
+					comNum++;
+					if(nums[k]<nums[k-h])
+						swap(nums,k,k-h);
+					else
+						break;
+				}
+			}
+		}
+		return comNum;
+	}
+	
+	public static int insertSort(int[] nums) {
+		int comNum=0;
+		for(int i=0;i<nums.length;i++) {
+			for(int j=i;j>0;j--) {
+				comNum++;
+				if(nums[j]<nums[j-1])
+					swap(nums,j,j-1);
+				else
+					break;
+			}
+		}
+		return comNum;
+	}
+	
+	public static void swap(int[] nums,int p,int q) {
+		int temp=nums[p];
+		nums[p]=nums[q];
+		nums[q]=temp;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			String[] fileName= {"data0.1024","data0.2048","data0.4096","data0.8192","data0.16384","data0.32768",
+					"data1.1024","data1.2048","data1.4096","data1.8192","data1.16384","data1.32768"};
+			for(int k=0;k<fileName.length;k++) {
+				// read the data from the file
+				File file=new File(fileName[k]);
+				FileReader fileReader = new FileReader(file);
+				
+				int size=Integer.valueOf(fileName[k].substring(6,fileName[k].length()));
+				int[] array=new int[size];
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				String line;
+				int index=0;
+				// to store the data in the file into a array
+				while ((line = bufferedReader.readLine()) != null) {
+					array[index]=Integer.valueOf(line);
+					index++;
+				}
+				
+				fileReader.close();
+				int[] tempArray=new int[array.length];
+				System.arraycopy(array, 0, tempArray, 0, array.length);
+				int shellRes=shellosrt(array);
+				int insertRes=insertSort(tempArray);
+				
+				System.out.println(shellRes+" comparisons made in shell sort in "+fileName[k]);
+				System.out.println(insertRes+" comparisons made in insert sort in "+fileName[k]);
+				System.out.println();
+				
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ Assignment2/src/Homework2_2.java
@@ -0,0 +1,112 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.naming.ldap.SortControl;
+
+public class Homework2_2 {
+	
+	private static int[] aux;
+	public static int countKTD(int[] nums) {
+		int res=0;
+		aux=new int[nums.length];
+		res=sort(nums, 0, nums.length-1);
+		return res;
+	}
+	
+	// This method sort the aux array and returns the number of inversions
+	public static int sort(int[] nums,int lo,int hi) {
+		int res=0;
+		if(hi>lo) {
+			// the mid index
+			int mid=lo+(hi-lo)/2;
+			// sort the left part
+			res+=sort(nums, lo, mid);
+			// sort the right part
+			res+=sort(nums, mid+1,hi);
+			// merge the two subarray and return the number of the inversions
+			res+=merge(nums, lo, mid, hi);
+		}
+		
+		return res;
+	}
+	
+	// This method sort the sorted array and returns the number of inversions
+	public static int merge(int[] nums,int lo,int mid,int hi) {
+		int res=0;
+		int i=lo,j=mid+1;
+		
+		for(int k=lo;k<=hi;k++) {
+			aux[k]=nums[k];
+		}
+		for(int k=lo;k<=hi;k++) {
+			if(i>mid)
+				nums[k]=aux[j++];
+			else if(j>hi)
+				nums[k]=aux[i++];
+			else if(aux[i]<aux[j]) {
+				nums[k]=aux[i++];
+			} else {
+				nums[k]=aux[j++];
+				res+=mid-i+1;
+			}
+		}
+		
+		return res;	
+	}
+	
+
+	public static void main(String[] args) {
+		try {
+			int[] result=new int[12];
+			int[] invNum=new int[12];
+			String[] fileName= {"data0.1024","data0.2048","data0.4096","data0.8192","data0.16384","data0.32768",
+					"data1.1024","data1.2048","data1.4096","data1.8192","data1.16384","data1.32768"};
+			for(int i=0;i<5;i++) {
+				for(int k=0;k<fileName.length;k++) {
+					// read the data from the file
+					File file=new File(fileName[k]);
+					FileReader fileReader = new FileReader(file);
+					
+					int size=Integer.valueOf(fileName[k].substring(6,fileName[k].length()));
+					int[] array=new int[size];
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+					String line;
+					int index=0;
+					// to store the data in the file into a array
+					while ((line = bufferedReader.readLine()) != null) {
+						array[index]=Integer.valueOf(line);
+						index++;
+					}
+					
+					fileReader.close();
+
+					long startTime=System.nanoTime();
+					int num=countKTD(array);
+					long endTime=System.nanoTime();
+					
+					result[k]+=(endTime-startTime);
+					invNum[k]+=num;
+					// output the result and the running time
+//					System.out.println(num+" inversions in the dataset: "+fileName[k]);
+//					System.out.println("Running time is: "+String.valueOf((endTime-startTime)/100)+" μs");
+//					System.out.println();
+				}
+
+			}
+			for(int i=0;i<12;i++) {
+				System.out.println(String.valueOf(invNum[i]/5) +" inversions in the dataset: ");
+				System.out.println("Running time is: "+String.valueOf(result[i]/1000)+" μs");
+				System.out.println();
+			}
+
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+//		 int arr[] = new int[]{1, 5, 4, 8, 10,2,6,9,3,7};
+//	     System.out.println("Number of inversions are " + countKTD(arr));
+
+	}
+
+}
