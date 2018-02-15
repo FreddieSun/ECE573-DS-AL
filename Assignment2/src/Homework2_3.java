84b858f031f4590907dc1a3878061052fa0f180f
FreddieSun <freddiesun95@gmail.com>
2018-02-14 20:24:08 -0500

    homework2_3

--- Assignment2/src/Homework2_3.java
+++ Assignment2/src/Homework2_3.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Homework2_3 {
 
@@ -15,7 +17,42 @@ public class Homework2_3 {
 				nums[i]=1111;
 		}
 		
+		countingSort(nums);
 		
+		for(int i=0;i<nums.length;i++)
+			System.out.println(nums[i]);
 	}
 
+	private static int getMax(int[] arr) {  
+	    int max = 0;  
+	    for (int ele : arr) {  
+	        if (ele > max) {  
+	            max = ele;  
+	        }  
+	    }  
+	    return max;  
+	}  
+	
+	public static void countingSort(int[] nums) {
+		int size=nums.length;
+		int max=getMax(nums);
+		
+		int[] count=new int[max+1];
+		int[] res=new int[size];
+		
+		for(int i=0;i<size;i++) {
+			++count[nums[i]];
+		}
+		
+		for(int i=1;i<max+1;i++)
+			count[i]+=count[i-1];
+		
+		for(int i=0;i<size;i++) {
+			res[count[nums[i]]-1]=nums[i];
+			--count[nums[i]];
+		}
+		
+		for(int i=0;i<size;i++)
+			nums[i]=res[i];
+	}
 }
