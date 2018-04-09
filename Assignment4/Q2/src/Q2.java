70241221c87e890d56a0ac3d7cf101b203c9c445
FreddieSun <freddiesun95@gmail.com>
2018-04-09 17:33:35 -0400

    Q2 finished

--- Assignment4/Q2/src/Q2.java
+++ Assignment4/Q2/src/Q2.java
@@ -1,5 +1,21 @@
-public class Q2 {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
 
+public class Q2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        // 读入
+        EdgeWeightedGraph ewg = new EdgeWeightedGraph("Q1.txt");
+
+        long start1 = System.currentTimeMillis();
+
+        KruskalMST kmst = new KruskalMST(ewg);
+
+        long end1 = System.currentTimeMillis();
+
+        long start2 = System.currentTimeMillis();
+        PrimMST pmst = new PrimMST(ewg);
+        long end2 = System.currentTimeMillis();
+
+        System.out.println(end1 - start1);
+        System.out.println(end2 - start2);
     }
 }
