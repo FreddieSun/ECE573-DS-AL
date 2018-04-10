685b63b866861f196a58fe65505c8890bd34958a
FreddieSun <freddiesun95@gmail.com>
2018-04-09 20:32:40 -0400

    Q3 finished

--- Assignment4/Q2/src/Q2.java
+++ Assignment4/Q2/src/Q2.java
@@ -17,5 +17,6 @@ public class Q2 {
 
         System.out.println(end1 - start1);
         System.out.println(end2 - start2);
+
     }
 }
--- Assignment4/Q3/src/Q3.java
+++ Assignment4/Q3/src/Q3.java
@@ -1,7 +1,43 @@
+import java.io.FileNotFoundException;
+
 public class Q3 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        EdgeWeightedDigraph ewd = new EdgeWeightedDigraph("Q3.txt");
+        int s = 0;
+        AcyclicSP sp = new AcyclicSP(ewd, s);
+        AcyclicLP lp = new AcyclicLP(ewd, s);
+
+        // find shortest path from s to each other vertex in DAG
+        System.out.println("Shortest Path: ");
+        for (int v = 0; v < ewd.V(); v++) {
+            if (sp.hasPathTo(v)) {
+                System.out.println(s + " to " + v + " " + sp.distTo(v));
+                System.out.print("The shortest path is as follows: ");
+                for (DirectedEdge e : sp.pathTo(v)) {
+                    System.out.println(e + "   ");
+                }
+            } else {
+                System.out.println(s + " to " + v + " no path");
+
+            }
+        }
+        System.out.println();
+
+        System.out.println("Longest Path");
+        for (int v = 0; v < ewd.V(); v++) {
+            if (lp.hasPathTo(v)) {
+                System.out.println(s + " to " + v + " " + lp.distTo(v));
+                System.out.print("The Longest path is as follows: ");
+                for (DirectedEdge e : lp.pathTo(v)) {
+                    System.out.println(e + "   ");
+                }
+            } else {
+                System.out.println(s + " to " + v + " no path");
+
+            }
+        }
     }
 
 
